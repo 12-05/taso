9bacfe21ccdb49853dac1e511badcba1ff796ef2
Yukun <y.liu@12-5.de>
2022-06-07 12:10:21 +0200

    split content

---
+++ layouts/split_content.php
@@ -0,0 +1,15 @@
+<div class="row">
+            <div class="contact_page">
+       
+    <?php if (get_sub_field('contactform_shortcode')): ?>
+        <div class="form">
+<?php the_sub_field('contactform_shortcode') ?>
+    </div>
+<?php endif; ?>
+    <?php if (get_sub_field('content')): ?>
+    <div class="content">
+        <?php the_sub_field('content') ?>
+    </div>
+    <?php endif; ?>
+</div>
+</div>
