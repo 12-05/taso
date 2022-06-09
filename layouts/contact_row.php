9c070a2e304f48e7a0cad711f65218b7773fa6e7
Yukun <y.liu@12-5.de>
2022-06-09 16:35:37 +0200

    de map

--- layouts/contact_row.php
+++ layouts/contact_row.php
@@ -10,7 +10,7 @@
     <?php if (get_sub_field('content')): ?>
     <div class="content">
         <?php the_sub_field('content') ?>
-        <iframe style='width:100%;border:none' src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2501.294323549839!2d7.058266400000002!3d51.176798!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47b8d47fe7a55285%3A0xaa2edfebee8d680!2sKleinenberger%20Str.%2014-16%2C%2042719%20Solingen!5e0!3m2!1szh-CN!2sde!4v1654592546791!5m2!1szh-CN!2sde" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+        <iframe style='width:100%;border:none' src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2501.2941422052295!2d7.05607771626915!3d51.17680134264135!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47b8d47fe7a55285%3A0xaa2edfebee8d680!2sKleinenberger%20Str.%2014-16%2C%2042719%20Solingen!5e0!3m2!1sde!2sde!4v1654785295415!5m2!1sde!2sde" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
     </div>
     <?php endif; ?>
 </div>
