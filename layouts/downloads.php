7214743c4bba8c04edf2822acdc2fdc96b354fec
Magnus Westhofen <m.westhofen@12-05.de>
2022-10-31 11:09:41 +0100

    Update downloads.php

--- layouts/downloads.php
+++ layouts/downloads.php
@@ -17,9 +17,10 @@
                         <div class='files' style='flex-wrap:wrap;max-width:800px;'> 
                         <?php foreach($unterdata['files'] as $file):?>
                         <div  style='display:flex;flex-direction:column;margin-right:32px;'>
-                                <?php if($firefox!==FALSE){}else{?>
-                                <iframe scrolling="no" src="<?php echo wp_get_attachment_url($file['file']); ?>" style="width: 100px;height:100%;border: none;"></iframe>
-                                <?php }?>
+				<?php 
+				$image = wp_get_attachment_image( $file['file'], 'medium' );
+				?>
+                              	<img src="<?php echo $image;?>" width="150" alt="Preview" />
                                 <a target='_blank' href='<?php echo wp_get_attachment_url($file['file']); ?>' style='max-width:120px;font-size:0.8rem'><?php echo $file['filename']?></a>
                         </div>
                         <?php endforeach;?>
