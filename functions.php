71dbf4db6e48d3cf6f964778727887cb445a75f2
Magnus Westhofen <m.westhofen@12-05.de>
2023-07-06 11:07:59 +0200

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,6 +1,12 @@
 <?php
 	
-wp_enqueue_style( 'taso-style', get_template_directory_uri().'/assets/css/min/style.min.css');
+
+function wpdocs_theme_name_scripts() {
+	wp_enqueue_style( 'taso-style', get_template_directory_uri().'/assets/css/min/style.min.css');
+
+}
+add_action( 'wp_enqueue_scripts', 'wpdocs_theme_name_scripts' );
+
 
 function wpb_custom_new_menu() {
 	register_nav_menus(
