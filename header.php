3c8ea5bcd8533468a34128a411c4e3283179597e
Magnus Westhofen <m.westhofen@12-05.de>
2023-07-06 11:37:21 +0200

    Update header.php

--- header.php
+++ header.php
@@ -50,7 +50,7 @@ bloginfo( 'title' );
 
 
 <?php if (get_field('anfrage','option')) { ?>
-<?php if(function_exists('pll_e') {?>
+<?php if(function_exists('pll_e')) {?>
 <div class="phone--box"><div class="phone--link"><?php  echo pll_e('... und alles ist möglich!') ?></div></div>
 	<?php } ?>
 <?php }else{
