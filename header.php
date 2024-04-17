cb87aaea7de01c1c1b870efcaa4b8e9a7f635808
Magnus Westhofen <m.westhofen@12-05.de>
2024-04-17 10:40:47 +0200

    test

--- header.php
+++ header.php
@@ -1,15 +1,15 @@
 <!DOCTYPE html>
-<html <?php language_attributes(); ?> class="no-js">
+<html <?php language_attributes();?> class="no-js">
 <head>
-<meta charset="<?php bloginfo( 'charset' ); ?>">
+<meta charset="<?php bloginfo('charset');?>">
 <meta name="viewport" content="width=device-width, initial-scale=1" />
 
-<?php if ( is_singular() && pings_open() ) { ?>
-<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
-<?php } ?>
+<?php if (is_singular() && pings_open()) {?>
+<link rel="pingback" href="<?php bloginfo('pingback_url');?>">
+<?php }?>
 
-<link href="https://fonts.12-05.com/roboto/style.css" rel="stylesheet">
-<script> 
+<link href="https://www.taso-solingen.de/wp-content/themes/taso/style.css" rel="stylesheet">
+<script>
 function myFunc(){
 const menu = document.getElementById("menu-menu-1");
 menu.classList.toggle("active");
@@ -17,16 +17,16 @@ menu.classList.toggle("active");
 </script>
 <?php wp_head();?>
 </head>
-<body <?php body_class(); ?>>
+<body <?php body_class();?>>
 <header id="header" class="clearfix" role="banner">
 <div class="header-inner">
 <h1 class="site-title">
-<a href="<?php echo esc_url( home_url( '/' ) ); ?>">
-<?php if ( get_field('logo' , 'option') ) { ?>
-<img src="<?php the_field('logo','option') ?>" alt="" width="<?php the_field('logo_size','option') ?>"/>
+<a href="<?php echo esc_url(home_url('/')); ?>">
+<?php if (get_field('logo', 'option')) {?>
+<img src="<?php the_field('logo', 'option')?>" alt="" width="<?php the_field('logo_size', 'option')?>"/>
 <?php } else {
-bloginfo( 'title' ); 
-} ?>
+    bloginfo('title');
+}?>
 </a>
 </h1>
 <nav class="navbar">
@@ -36,33 +36,33 @@ bloginfo( 'title' );
 <span class="hamburger-line hamburger-line-bottom" aria-hidden="true"></span>
 </button>
 <?php
-         
-            wp_nav_menu( array(
-                'theme_location' => 'header-menu', 
-                'container' => 'menu-xxx-container', 
-                'menu_class' => 'nav navbar-nav menu__list ', 
-                'menu' => 'Hauptmenü'
-              )); 
-              
-?> 
 
-<?php if(function_exists('pll_e')) {?>
+wp_nav_menu(array(
+    'theme_location' => 'header-menu',
+    'container' => 'menu-xxx-container',
+    'menu_class' => 'nav navbar-nav menu__list ',
+    'menu' => 'Hauptmenü',
+));
 
-<?php if (get_field('anfrage','option')) { ?>
-<div class="phone--box"><div class="phone--link"><?php  echo pll_e('... und alles ist möglich!') ?></div></div>
-<?php }else{
-    echo "" ; 
-} ?>
-				    	<?php } ?>
+?>
+
+<?php if (function_exists('pll_e')) {?>
+
+<?php if (get_field('anfrage', 'option')) {?>
+<div class="phone--box"><div class="phone--link"><?php echo pll_e('... und alles ist möglich!') ?></div></div>
+<?php } else {
+    echo "";
+}?>
+				    	<?php }?>
 
 </nav>
 </div>
 </header>
 <div id="wrapper" class="clearfix">
-    <?php if (get_field('telefon','option') && get_field('phone-icon','option') && get_field('e-mail','option') && get_field('mail-icon','option')  ): ?>
+    <?php if (get_field('telefon', 'option') && get_field('phone-icon', 'option') && get_field('e-mail', 'option') && get_field('mail-icon', 'option')): ?>
 <div class="quick-contact-box">
-        <a  href="tel:<?php the_field('telefon', 'option') ?>"><img src="<?php the_field('phone-icon', 'option')?>" /></a>
-         <a href="mailto:<?php the_field('e-mail', 'option') ?>"><img src="<?php the_field('mail-icon', 'option')?>" /></a>
+        <a  href="tel:<?php the_field('telefon', 'option')?>"><img src="<?php the_field('phone-icon', 'option')?>" /></a>
+         <a href="mailto:<?php the_field('e-mail', 'option')?>"><img src="<?php the_field('mail-icon', 'option')?>" /></a>
     </div>
 
-    <?php endif; ?>
+    <?php endif;?>
