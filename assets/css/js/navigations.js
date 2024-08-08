78f44fa12767e2fd92fb0079a24c9432f6bffe7f
Magnus Westhofen <m.westhofen@12-05.de>
2024-08-08 09:25:05 +0200

    test

--- assets/css/js/navigations.js
+++ assets/css/js/navigations.js
@@ -1,6 +1,8 @@
 jQuery(document).ready(function ($) {
-	jQuery(".menu-item-has-children").click(function (e) {
+	jQuery(".menu-item-has-children > a").click(function (e) {
 		e.preventDefault();
+	});
+	jQuery(".menu-item-has-children").click(function (e) {
 		jQuery(this).find(".sub-menu").toggle();
 	});
 });
